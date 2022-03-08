cc0aa481b580deecfbe4a0c89e07ac48a8ea22e6
Mario Alina <74239389+MarioEpk@users.noreply.github.com>
2022-03-08 15:30:20 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pyramid pyramid = new Pyramid(20);
+        }
+
+        
+    }
+}
---
+++ Pyramid.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Pyramid
+    {
+        int numOfAsterixs = 1;
+        int totalRows;
+
+        public Pyramid(int nRows)
+        {
+            int totalWhiteSpaces = nRows + 1;
+            totalRows = nRows;
+
+
+            while (totalRows > 0)
+            {
+                for (int i = totalWhiteSpaces; i > 0; i -= 1)
+                {
+                    Console.Write(" ");
+                }
+
+
+                PrintAsterix(numOfAsterixs);
+                Console.WriteLine();
+                numOfAsterixs += 2;
+                totalRows -= 1;
+                totalWhiteSpaces -= 1;
+            }
+        }
+
+        private void PrintAsterix(int n)
+        {
+            while (n > 0)
+            {
+                if (n == 1 || n == numOfAsterixs || totalRows == 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                Console.Write("*");
+                Console.ForegroundColor = ConsoleColor.White;
+                n -= 1;
+            }
+        }
+    }
+}
